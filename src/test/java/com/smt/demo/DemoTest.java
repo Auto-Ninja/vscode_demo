6781f6d01643efe27714799fdcf207ec4f91a3e3
smithajojo <smithajojo@live.com>
2024-11-24 21:06:33 +0000

    Photo Moved to Trash

--- src/test/java/com/smt/demo/DemoTest.java
+++ src/test/java/com/smt/demo/DemoTest.java
@@ -1,6 +1,7 @@
 package com.smt.demo;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,7 +13,7 @@ import org.openqa.selenium.interactions.Actions;
 
 public class DemoTest {
     
-    @Test
+    @Test()
     public void DragAndDrop() throws InterruptedException
     {
         ////https://www.youtube.com/watch?v=DxBWuX4qR_E
@@ -34,6 +35,34 @@ public class DemoTest {
         System.out.println("Performing Drag and Drop");
         Thread.sleep(3000);
         Assertions.assertEquals("Dropped!", driver.findElement(By.id("droppable")).getText());
+        driver.quit();
+    }
+
+    @Test()
+    public void DragAndDropImage() throws InterruptedException
+    {
+        ////https://www.youtube.com/watch?v=DxBWuX4qR_E
+        System.out.println("Test run sucessfully -------------------------------------");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+        driver.get("https://jqueryui.com/droppable");
+        driver.findElement(By.xpath("//*[@id=\"content\"]/div[1]/ul/li[5]/a")).click();
+
+        Thread.sleep(3000);
+
+        driver.switchTo().frame(driver.findElement(By.xpath("//*[@id=\"content\"]/iframe")));
+        List<WebElement> draggables =  driver.findElements(By.xpath("//*[@id=\"gallery\"]/li"));
+        WebElement droppable = driver.findElement(By.id("trash"));
+        for(WebElement draggable : draggables)
+        {
+           
+            new Actions(driver)
+                    .dragAndDrop(draggable, droppable)
+                    .perform(); 
+            System.out.println("Performing Drag and Drop");
+            Thread.sleep(3000);
+        }
         driver.quit();
     }
 }
